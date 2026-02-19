04ebd0d70c14ec7ba17dee5a128430efb1ab45fe
Julien Russo <julien.russo@calzedonia.it>
2026-02-19 14:58:11 +0100

    Implemented replace endpoint for entity "Product".

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -6,7 +6,7 @@ var router = express.Router();
 const supabase = supa.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 // POST /product => 201
-router.post('/', async function (req, res, next) {
+router.post('/', async function (req, res) {
 	const newProduct = {
 		name: req.body.name,
 		price: req.body.price
@@ -42,7 +42,7 @@ router.post('/', async function (req, res, next) {
 });
 
 // GET /product => Product[]
-router.get('/', async function (req, res, next) {
+router.get('/', async function (_, res) {
 	const { data, error } = await supabase
 		.from('products')
 		.select();
@@ -51,7 +51,7 @@ router.get('/', async function (req, res, next) {
 });
 
 // GET /product/{id} => Product
-router.get('/:id', async function (req, res, next) {
+router.get('/:id', async function (req, res) {
 	const { data, error } = await supabase
 		.from('products')
 		.select()
@@ -60,5 +60,45 @@ router.get('/:id', async function (req, res, next) {
 	if (data.length === 0) res.status(404);
 	res.json(data[0]);
 });
+
+// PUT /product/{id} => Product
+router.put('/:id', async function (req, res) {
+	const newProduct = {
+		id: req.params.id,
+		name: req.body.name,
+		price: req.body.price
+	};
+	if (newProduct.name === undefined) {
+		res
+			.status(400)
+			.json({"errorMessage": "Missing product name"});
+	}
+	if (newProduct.price === undefined) {
+		res
+			.status(400)
+			.json({"errorMessage": "Missing product price"});
+	}
+	const { data, error } = await supabase
+		.from('products')
+		.update(newProduct)
+		.eq("id", req.params.id)
+		.select();
+	if (error !== null) {
+		res
+			.status(500)
+			.json(
+				{
+					"errorMessage": "Server error",
+					"details": error.details
+				}
+			);
+	}
+	console.log("AAAAAAAAA", data);
+	res
+		.status(200)
+		.location(`/product/${data[0].id}`)
+		.json(data);
+})
+
 
 module.exports = router;
