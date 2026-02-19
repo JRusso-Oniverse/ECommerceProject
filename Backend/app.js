0fdb7abe34b2494652b7c502d859944e5c419f0d
Julien Russo <julien.russo@calzedonia.it>
2026-02-19 09:50:59 +0100

    Code cleanup.

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,9 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require("dotenv").config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require("dotenv").config();
 
 var productRouter = require('./routes/product');
 var usersRouter = require('./routes/users');
@@ -22,8 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/product', productRouter);
-app.use('/users', usersRouter);
-app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
