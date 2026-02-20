c311c4e95a02cd23ff8c53b3a55e8695569fe521
Riccardo Raffa <riccardo.raffa@calzedonia.it>
2026-02-20 12:18:06 +0100

    orders routes maybe def

--- Backend/routes/orders.js
+++ Backend/routes/orders.js
@@ -6,61 +6,26 @@ var router = express.Router();
 const supabase = supa.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 
+//ORDERS entity: id(PK), user_id(FK), product_id(FK), count
 
 
-//get * orders
-router.get('/', async function (req, res, next) {
-	const { data, error } = await supabase;
-	if (req.params.role == "admin") {
-		const { data, error } = await supabase
-			.from('orders')
-			.select();
-
-		if (error !== null)
-			res.status(500);
-
-		res.send(data);
-
-	} else {
-		const { data, error } = await supabase
-			.from('orders')
-			.select()
-			.eq(req.params.id, 'id');
-
-		if (error !== null)
-			res.status(500);
-
-		res.send(data);
-	}
-});
-
-
-//get orders by user/id
-router.get('/:id', async function (req, res, next) {
-
+//get all orders
+router.get('/', async function (req, res) {
 	const { data, error } = await supabase
-		.from('users')
-		.select()
-		.eq('id', req.params.id)
-		.join('orders');
-
-	if (error !== null)
-		res.status(500);
-
-	if (data.length === 0)
-		res.status(404);
-
-	res.send(data);
+		.from('orders')
+		.select();
+	if (error !== null) res.status(500);
+	res.json(data);
 });
 
 
-// get order by /order/id
+//get orders by user id
 router.get('/:id', async function (req, res, next) {
 
 	const { data, error } = await supabase
 		.from('orders')
 		.select()
-		.eq('id', req.params.id);
+		.eq('id', req.params.user_id);
 
 	if (error !== null)
 		res.status(500);
@@ -68,50 +33,62 @@ router.get('/:id', async function (req, res, next) {
 	if (data.length === 0)
 		res.status(404);
 
-	res.send(data[0]);
+	res.json(data);
 });
 
 
-
-//ORDERS objs: id(PK), user_id(FK), product_id(FK), count
 
 
 
 //create new order
-router.post('/orders', async function (req, res, next) {
-
-	const orderPayload = new Order({
-		id: req.body.id,
-		user_id: req.users.id,
+router.post('/', async function (req, res) {
+	const newOrder = {
+		user_id: req.body.user_id,
 		product_id: req.body.product_id,
-		count: req.body.count,
-	});
+		count: req.body.count
+	};
 
-	const new_order = await orderPayload.save();
-	res.status(201).send({
-		message: "new order created!",
-		data: new_order
-	});
-
-	console.error("err");
-});
-
-
-//delete order
-router.delete('/', async function (req, res, next) {
-
-	const del_order = await Order.findOne({
-		id: req.params.id
-	});
-
-	if (del_order) {
-		const deleted_order = await del_order.remove();
-		res.send(deleted_order);
-	} else {
-		res.status(404).send("order not found.")
+	if (newOrder.user_id === undefined) {
+		res
+			.status(400)
+			.json({ "errorMessage": "Missing user id" });
+		return;
 	}
-	
+
+	if (newOrder.product_id === undefined) {
+		res
+			.status(400)
+			.json({ "errorMessage": "Missing product id" });
+		return;
+	}
+	if (newOrder.count === undefined) {
+		res
+			.status(400)
+			.json({ "errorMessage": "Missing products count" });
+		return;
+	}
+	const { data, error } = await supabase
+		.from('orders')
+		.insert(newOrder)
+		.select();
+	if (error !== null) {
+		res
+			.status(500)
+			.json(
+				{
+					"errorMessage": "Server error",
+					"details": error.details
+				}
+			);
+		return;
+	}
+	res
+		.status(201)
+		.location(`/order/${data[0].id}`)
+		.json(data);
+	return;
 });
+
 
 
 module.exports = router;
