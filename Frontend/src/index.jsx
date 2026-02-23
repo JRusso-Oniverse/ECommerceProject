bf03f4d470139e5705753a1954a7782219b9c49c
Julien Russo <julien.russo@calzedonia.it>
2026-02-23 17:25:26 +0100

    Alfonso

---
+++ Frontend/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Index from './components/Index/Index';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+);
