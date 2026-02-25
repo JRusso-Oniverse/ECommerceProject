84017c9552894d4a1f929448a154d1e60ccc90bf
Riccardo Raffa <riccardo.raffa@calzedonia.it>
2026-02-25 09:19:17 +0100

    added pdp css nd jsx

---
+++ Frontend/src/components/PDP/PDP.jsx
@@ -0,0 +1,60 @@
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+import './PDP.css';
+
+
+
+function PDP() {
+    return <>
+        <Header />
+
+        <main>
+            <h1>Prodotto</h1>
+
+            <div id="product">
+
+                <img src="imgprova.png" alt="prod1" id="image" />
+
+                <div id="containflex">
+
+                    <div id="description">
+                        <strong>DESCRIPTION</strong>
+
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                        hahahahahahahahahahahahaha
+                    </div>
+
+                    <div id="flexbutt">
+                        <button id="goback">
+                            GOBACK B
+                        </button>
+                        <button id="addtocart">
+                            CART B
+                        </button>
+                    </div>
+                    
+                </div>
+            </div>
+        </main>
+
+        <Footer />
+    </>
+}
+
+
+
+
+export default PDP;
